9aeddf4a5c2e66a4fa6b39cf82998114827e228a
Romain Poyault <romain.poyault@outlook.fr>
2023-01-24 23:12:48 +0100

    [ADD] POI card + Display POI

--- src/pages/PoiPage.jsx
+++ src/pages/PoiPage.jsx
@@ -1,9 +1,59 @@
-import { Typography } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  Grid, Box, CircularProgress, Typography,
+} from '@mui/material';
+import getAllPOI from '../services/POIQueries';
+import PoiCard from '../components/PoiCard';
 
 function PoiPage() {
+  const [POIListData, setPOIListData] = useState(undefined);
+  useEffect(() => {
+    async function fetchData() {
+      const fetchedData = await getAllPOI();
+      if (fetchedData !== null) {
+        setPOIListData(fetchedData);
+      } else {
+        setPOIListData(null);
+      }
+    }
+    fetchData();
+  }, []);
+
+  if (POIListData === undefined) {
+    return (
+      <CircularProgress />
+    );
+  }
+
   return (
-    <Typography>POI</Typography>
+    <Box
+      display="flex"
+      justifyContent="center"
+      sx={{
+        marginTop: '50px', flexGrow: 1,
+      }}
+    >
+      { POIListData !== undefined && POIListData.length !== 0
+        && (
+          <Grid container spacing={2} sx={{ width: '80vw', maxWidth: '1500px' }} display="flex">
+            {
+              Object.keys(POIListData).map((key) => (
+                <Grid key={key} item xs={6} sx={{ width: '35vw', minWidth: '300px' }}>
+                  {console.log(POIListData[key])}
+                  <PoiCard id={key} data={POIListData[key]} />
+                </Grid>
+              ))
+            }
+          </Grid>
+        )}
+      {
+        POIListData !== undefined && POIListData.length === 0
+        && <Typography>Il n&apos;y a aucun groupe</Typography>
+      }
+      {
+        POIListData === undefined && <CircularProgress />
+      }
+    </Box>
   );
 }
 
