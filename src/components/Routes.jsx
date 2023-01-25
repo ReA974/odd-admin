59cdb5d91bee62e0bea0bdb7ddbb9795342f3805
agirar05 <alexandre.girard@etudiant.univ-lr.fr>
2023-01-25 15:24:43 +0100

    [FIX] Delete dead code

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,7 +1,4 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-// import PropTypes from 'prop-types';
-// import { CircularProgress } from '@mui/material';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, Outlet } from 'react-router-dom';
 import { auth } from '../services/firestore.service';
@@ -17,55 +14,3 @@ export function DisconnectedRoute() {
 
   return user ? <Navigate to="/poi" /> : <Outlet />;
 }
-
-// export function PrivateRoute({ children, ...rest }) {
-//   const [user, loading] = useAuthState(auth);
-//   if (loading) {
-//     return (
-//       <div className="App-centered">
-//         <CircularProgress sx={{ marginTop: '50px' }} />
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <Route
-//       {...rest}
-//       render={() => (user ? (
-//         children
-//       ) : (
-//         <Navigate replace to="/poi" />
-//       ))}
-//     />
-//   );
-// }
-// PrivateRoute.propTypes = {
-//   children: PropTypes.element.isRequired,
-// };
-
-// export function DisconnectedRoute({ children, ...rest }) {
-//   const [user, loading] = useAuthState(auth);
-
-//   if (loading) {
-//     return (
-//       <div className="App-centered">
-//         <CircularProgress sx={{ marginTop: '50px' }} />
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <Route
-//       {...rest}
-//       render={() => (!user ? (
-//         children
-//       ) : (
-//         <Navigate replace to="/poi" />
-//       ))}
-//     />
-//   );
-// }
-
-// DisconnectedRoute.propTypes = {
-//   children: PropTypes.element.isRequired,
-// };
